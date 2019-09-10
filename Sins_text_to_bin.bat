2490067f1f33205649c0a80892e8b9f47b1636ef
Corey <ThrawnsRevenge@gmail.com>
2019-09-10 15:42:11 -0400

    AI Should Recruit Minor Factions

---
+++ Sins_text_to_bin.bat
@@ -0,0 +1,4 @@
+for %%a in (*.entity) do ConvertData_Rebellion entity %%a %%a 
+for %%a in (*.particle) do ConvertData_Rebellion particle %%a %%a 
+for %%a in (*.mesh) do ConvertData_Rebellion mesh %%a %%a 
+for %%a in (*.brushes) do ConvertData_Rebellion brushes %%a %%a 
